4718c7843335a0c29f9990d646e7f8859241fbc0
kindrew <kindrew99@gmail.com>
2023-12-14 13:23:28 +0000

    Upgrade

---
+++ android/app/src/main/kotlin/kin/com/id_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package kin.com.id_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int kinlevel = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("Kin ID Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            kinlevel++;
+          });
+        },
+          child:Icon(Icons.add)),
+        back
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/thumb.jpg"),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+            color: Colors.grey[800],),
+            Text("NAME",
+            style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 2.0,
+            ),),
+            SizedBox(height: 10.0),
+            Text("Kin Drew II",
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),),
+            SizedBox(height: 30.0),
+            Text("Current Kin Level",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),),
+            SizedBox(height: 10.0),
+            Text("$kinlevel",
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),),
+            SizedBox(height: 30.0),
+
+            Row(
+              children: [
+                Icon(Icons.mail,
+                color: Colors.grey,
+                ),
+                SizedBox(width: 10.0,),
+                Text("Kindrew@kin.com",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18.0,
+                  letterSpacing: 1.0,
+                ),)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
